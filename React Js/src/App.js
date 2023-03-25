71f3cef957bed45a834c8c3cf3e270cb9c1f977f
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-03-25 15:32:34 +0530

    Update App.js

--- React Js/src/App.js
+++ React Js/src/App.js
@@ -2,6 +2,7 @@
 import React,{Component} from 'react';
 import Table from './Table';
 import Form from './Form';
+import ApiExample from './ApiExample';
 
 class App extends Component {
   state = {
