3c66ed25805717081f513a088fdd813791edd9ed
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-04-02 10:02:02 +0530

    Add files via upload

---
+++ Nodejs/file1.js
@@ -0,0 +1,46 @@
+// const abc = (name, callback) => {
+//     console.log(`hello ${name}`);
+//     callback();
+//     }
+//     const abcd = () => {
+//     console.log("hello!");
+//     }
+//     abc("Sikha ", abcd);
+
+//     //create and write
+//     const fsa = require("fs");
+//     fsa.writeFileSync("filename.txt", 'This is file1');
+
+//     //add element to file
+//     fsa.appendFileSync("filename.txt","\nThis is append file");
+
+//     //read a file
+//     fsa.readFileSync("filename.txt");
+//     const read = fsa.readFileSync("filename.txt");
+//     console.log(read);
+
+//     //convert buffer to string
+//     const od = read.toString();
+//     console.log(od);
+
+//     //another way to convert buffer to string
+//     fsa.readFileSync("filename.txt");
+//     const print = fsa.readFileSync("filename.txt","UTF-8");
+//     console.log(print);
+
+    //ASynchronous 
+    const a=require("fs");
+    a.writeFile("a.txt","I am the original data",(err)=>{
+    console.log("file is created");
+    console.log(err);
+});
+
+//append data
+a.appendFile("a.txt",": hello i am added",(err)=>{
+    console.log("task completed");
+});
+
+//Read file data
+a.readFile("a.txt","UTF-8",(err,data)=>{
+    console.log(data);
+});
